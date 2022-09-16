cc49a736ce88e89d3a799061d9f06ea6e9c0d1bf
BulatovaTati <102695106+BulatovaTati@users.noreply.github.com>
2022-09-16 19:23:24 +0000

    Deploying to gh-pages from @ BulatovaTati/Donats@ff3b50a3e1f8caaadb5f4efc5a7ced276cccf663 🚀

--- index.99810940.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),document.body.classList.toggle("is-menu-shown")}))}();
-//# sourceMappingURL=index.99810940.js.map
--- index.c2c460fc.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(()=>{const s="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!s),t.classList.toggle("is-open"),document.body.classList.toggle("is-menu-shown")}))})();
-//# sourceMappingURL=index.c2c460fc.js.map
