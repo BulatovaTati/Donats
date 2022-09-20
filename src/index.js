46f4e6dca64d124f827077eea3a453ba8b5758b9
BulatovaTati <kohodatat@gmail.com>
2022-09-20 18:36:18 +0300

    add swiper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './js/agreement';
 import './js/animation';
-import './js/flip-card';
+import './js/swiper';
+
 import './js/menu';
 import './js/modal';
 import './js/to-top';
+
 import './js/readmore-prod';
 import './js/readmore-about';
 import './js/smooth-scroll';
-import './js/swiper';
