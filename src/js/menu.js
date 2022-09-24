2482d5397ae464e9e2c712e88f871b46f8b71d54
BulatovaTati <kohodatat@gmail.com>
2022-09-24 21:07:44 +0300

    try to fix menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,81 +1,22 @@
 (() => {
   const menuBtns = document.querySelectorAll('[data-menu-button]');
-  const navLinks = document.querySelector('.menu__container');
+  // button <==
+  const navLinks = document.querySelector('[data-menu]');
+  //меню <====
+  const listOfLink = document.querySelector('.header__list');
+  // list <====
 
-  menuBtns.forEach(el =>
-    el.addEventListener('click', () => {
-      const expanded =
-        menuBtns.getAttribute('aria-expanded') === 'true' || false;
+  menuBtns.forEach(el => el.addEventListener('click', onClickMenu));
+  function onClickMenu(e) {
+    document.body.classList.toggle('is-menu-shown');
 
-      menuBtns.classList.toggle('is-open');
-      menuBtns.setAttribute('aria-expanded', !expanded);
-
-      menuBtns.classList.toggle('is-open');
-      document.body.classList.toggle('is-menu-shown');
-    })
-  );
+    if (e.target.nodeName === 'BUTTON') {
+      console.log('e.currentTarget: ', e.currentTarget);
+      // в карент button через раз попадає
+      console.log('Target: ', e.target);
+      // в таргет  svg
+      e.target.classList.toggle('is-open');
+      // Якщо написати  e.currentTarget тоді меню не закривається не знімаються класи
+    }
+  }
 })();
-
-// // const menuBtnRef = document.querySelector('[data-menu-button]');
-// // const mobileMenuRef = document.querySelector('[data-menu]');
-// // const navLinks = document.querySelector('.header__list');
-// // const navLinksMenu = document.querySelector('#menu__container');
-
-// // menuBtnRef.addEventListener('click', onMenuHandleClick);
-
-// // function onMenuHandleClick() {
-// //   const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-// //   menuBtnRef.classList.toggle('is-open');
-// //   menuBtnRef.setAttribute('aria-expanded', !expanded);
-// //   document.body.classList.toggle('is-menu-shown');
-
-// //   if (navLinks) {
-// //     navLinks.addEventListener('click', onMenuLinkClick);
-// //   }
-// //   // if (navLinksMenu) {
-// //   //   navLinks.addEventListener('click', onMenuHandleClick);
-// //   // }
-// // }
-
-// // function onMenuLinkClick(e) {
-// //   e.preventDefault();
-
-// //   if (e.target.nodeName !== 'A') return;
-// //   console.log('e.target: ', e.target);
-
-// //   const getAttribute = e.target.getAttribute('data-route');
-// //   const getElemId = document.getElementById(getAttribute);
-
-// //   const scrollToId = getElemId.getBoundingClientRect().top + scrollY;
-
-// //   window.scrollTo({
-// //     top: scrollToId,
-// //     behavior: 'smooth',
-// //   });
-// // }
-
-// const menuBtnRef = document.querySelector('[data-menu-button]');
-// const navLinks = document.querySelector('.header__list');
-
-// menuBtnRef.addEventListener('click', onClickMenu);
-
-// function onClickMenu() {
-//   const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-//   menuBtnRef.classList.toggle('is-open');
-//   menuBtnRef.setAttribute('aria-expanded', !expanded);
-//   document.body.classList.toggle('is-menu-shown');
-// }
-
-// navLinks.addEventListener('click', check);
-
-// function check(e) {
-//   e.preventDefault();
-//   if (e.target.nodeName !== 'A') return;
-//   onClickMenu();
-// }
-
-// // mobileMenuRef.addEventListener('click', () => {
-// //   onClickMenu();
-// // });
