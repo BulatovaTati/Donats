1f0b5b49b34c4cc4886142d526115c1917c19ae0
BulatovaTati <kohodatat@gmail.com>
2022-09-25 18:40:48 +0300

    finish project

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,27 +1,3 @@
-// (() => {
-//   const menuBtns = document.querySelectorAll('[data-menu-button]');
-//   // button <==
-//   const navLinks = document.querySelector('[data-menu]');
-//   //меню <====
-//   const listOfLink = document.querySelector('.header__list');
-//   // list <====
-
-//   menuBtns.forEach(el => el.addEventListener('click', onClickMenu));
-//   function onClickMenu(e) {
-//     document.body.classList.toggle('is-menu-shown');
-//     e.currentTarget.classList.toggle('is-open');
-
-//     if (e.target.nodeName === 'BUTTON') {
-//       console.log('e.currentTarget: ', e.currentTarget);
-//       // в карент button через раз попадає
-//       console.log('Target: ', e.target);
-//       // в таргет  svg
-//       e.target.classList.toggle('is-open');
-//       // Якщо написати  e.currentTarget тоді меню не закривається не знімаються класи
-//     }
-//   }
-// })();
-
 (() => {
   const menuBtns = document.querySelectorAll('[data-menu-button]');
   const menuBtn = document.querySelector('[data-menu-button]');
